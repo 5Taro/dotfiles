60d3e57eb79fef64b5e2b6d43fcedb0dc57b855d
5Taro <tarou2503@i.softbank.jp>
2022-01-29 19:35:09 +0900

    update

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,9 +1,9 @@
 #vim
-alias vz='vim ~/.zshrc'
-alias vv='vim ~/.vimrc'
-alias vtoml='vim ~/.vim/dein/dein.toml'
-alias vp='vim -p'
 alias v='vim'
+alias vp='vim -p'
+alias vtoml='vim ~/.vim/dein/dein.toml'
+alias vv='vim ~/.vimrc'
+alias vz='vim ~/.zshrc'
 
 #cd
 alias ....='cd ../../../'
@@ -12,24 +12,25 @@ alias dot='cd ~/dotfiles'
 
 #ls
 alias l='ls --color=auto'
-alias ls='ls --color=auto'
 alias la='ls -la --color=auto'
 alias ll='ls -l --color=auto'
+alias ls='ls --color=auto'
 alias lR='ls -R --color=auto'
 
 #git
-alias gs='git status'
 alias ga='git add'
 alias gc='git commit'
 alias gcm='git commit -m'
+alias gd='git diff'
 alias gp='git push'
+alias gs='git status'
 
 #others
-alias sz='source ~/.zshrc'
-alias mkdircd='(){mkdir $1 && cd $1}'
-alias m='make'
 alias c='clear'
 alias grep='grep --color=auto'
+alias m='make'
+alias mkdircd='(){mkdir $1 && cd $1}'
+alias sz='source ~/.zshrc'
 
 #global alias
 alias -g C='| clip.exe'
